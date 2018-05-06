68dd7edc9a2685b8bd69e8077421af87a1263b92
TomokiNishishimura <tomokichigai@gmail.com>
2018-05-06 08:53:54 +0000

    ログイン機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,24 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
   
   def new
   end
   
   def create
-    Tweet.create(tweet_params)
-    binding.pry
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
   
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text, :user_id)
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get   'tweets'      =>  'tweets#index'     #ツイート一覧画面 
-  get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
-  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  devise_for :users
+  root   'tweets#index'
+  get    'tweets'      =>  'tweets#index'     #ツイート一覧画面 
+  get    'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
+  post   'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  delete 'tweet/:id'   =>  'tweets#destroy'
+  get   'users/:id'    =>  'users#show'    #Mypageへのルーティング
 end
